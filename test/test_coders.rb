9a00420b98dfbc830d7e32267bf35ff05a1cfa53
Nathan Chen <miku@maimoe.com>
2015-04-26 00:39:13 +0800

    test case

--- test/test_coders.rb
+++ test/test_coders.rb
@@ -3,18 +3,18 @@ require 'rainbow'
 require 'thor'
 require 'minitest/autorun'
 
-Dir['./../helper/*.rb'].each { |file| require file }
-Dir['./../core/*.rb'].each { |file| require file }
-Dir['./../events/*.rb'].each { |file| require file }
-Dir['./../companies/*.rb'].each { |file| require file }
-Dir['./../projects/*.rb'].each { |file| require file }
+Dir['./helper/*.rb'].each { |file| require file }
+Dir['./core/*.rb'].each { |file| require file }
+Dir['./events/*.rb'].each { |file| require file }
+Dir['./companies/*.rb'].each { |file| require file }
+Dir['./projects/*.rb'].each { |file| require file }
 
 class TestCoders < Minitest::Test
   def setup
   end
 
   def test_work_would_work
-    Dir['./../coders/*.rb'].each do |file|
+    Dir['./coders/*.rb'].each do |file|
       require file
       class_name = file.split('/').last.split('.').first.camelize
       coder = Object.const_get(class_name).new
