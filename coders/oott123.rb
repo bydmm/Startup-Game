f6777dfe1e3d4ce27311cf58d12f789b1e0dda6b
oott123 <ip.192.168.1.1@qq.com>
2015-04-25 21:59:21 +0800

    完成 flags 机制

--- coders/oott123.rb
+++ coders/oott123.rb
@@ -4,6 +4,7 @@ class Oott123 < Coder
     @type = types.sample
     @week = 0
     @month = 0
+    @flags = {}
   end
 
   def name
@@ -55,8 +56,13 @@ class Oott123 < Coder
   def work(remain_difficulty)
     # 检查是否黑心老板
     if @week / 4 > @month
-      puts "#{name}表示自从上次解雇后并无心工作，本周并无进度。"
-      return remain_difficulty
+      # 检查良心发现
+      if rand(100) > 30
+        puts "#{name}表示自从上次解雇后并无心工作，本周并无进度。"
+        return remain_difficulty
+      else
+        @month = @week / 4 # 修正工龄，虽然会有一些问题，不过凑合吧……
+      end
     end
     productivity = rand(-100...100) # 遇到困难的比例是相同的
     if productivity > 0
@@ -68,7 +74,7 @@ class Oott123 < Coder
         productivity = productivity
       end
     end
-    productivity += rand(-50...50)  # 天有不测风云，加入并非能力可控因素
+    productivity = productivity + rand(-50...50)  # 天有不测风云，加入并非能力可控因素
     puts work_description(productivity)
     remain_difficulty -= productivity
     @week = @week + 1
@@ -84,10 +90,14 @@ class Oott123 < Coder
         "#{name}连夜加班，项目进度推进了 %d。",
         "#{name}暴打了 PM 一顿，项目推进了 %d。",
         "#{name}得到同事的真传，项目进度推进了 %d。",
-        "#{name}运用敏捷开发策略，项目进度推进了 %d。",
-        { :text => "#{@name}开始发展了一段办公室恋情，同时项目进度也推进了 %d。",
-          :flag => [{:id => 'fall_in_love_with_fellow', :op => :set}, {:id => 'fall_in_love', :op => :set}] }
+        "#{name}运用敏捷开发策略，项目进度推进了 %d。"
         ])
+      if !has_flag?('fall_in_love')
+        descriptions.concat([
+          { :text => "#{@name}开始发展了一段办公室恋情，同时项目进度也推进了 %d。",
+            :flag => [{:id => 'fall_in_love_with_fellow', :op => :set}, {:id => 'fall_in_love', :op => :set}] }
+          ])
+      end
     else
       descriptions.concat([
         "#{name}遇到了一些瓶颈，项目进度被推迟了 %d。",
@@ -99,16 +109,79 @@ class Oott123 < Coder
         "#{name}接了一些私活，项目进度被推迟了 %d。"
         ])
     end
+    # flags 情况
+    if has_flag?('fall_in_love')
+      if productivity > 0
+        descriptions.concat([
+          { :text => "#{name}的女朋友这周每天都到公司给他送爱心便当，项目进度推进了 %d。",
+            :flag => {:id => 'love_point', :op => :inc} },  # 然而 love_point 并没有什么卵用
+          { :text => "#{name}和女朋友一起吃喝玩乐，工作效率提升，项目进度推进了 %d。", 
+            :flag => {:id => 'love_point', :op => :inc} }
+          ])
+      else
+        descriptions.concat([
+          # o(￣ヘ￣o＃)哼你们就笑吧
+          { :text => "#{name}分手了，项目进度被推迟 %d 。",
+            # ps. 可能需要清除其它恋爱关系。
+            :flag => [{:id => 'fall_in_love_with_fellow', :op => :clear},
+                      {:id => 'fall_in_love', :op => :clear},
+                      {:id => 'love_point', :op => :clear}]}
+          ])
+      end
+    end
     description = descriptions.sample
     if description.is_a?(Hash)
-      flags = description[:flag]
-      if not flags.is_a?(Array)
-        flags = [flags]
-      end
-      # TODO: 处理 flags
+      puts description[:flag]
+      produce_flags(description[:flag])
+      puts @flags
       description = description[:text]
     end
     description % productivity.abs
+  end
+
+  # Flags 相关
+  
+  def has_flag?(id)
+    @flags.has_key?(id)
+  end
+
+  def get_flag(id)
+    if has_flag?(id)
+      @flags[id]
+    else
+      false
+    end
+  end
+
+  def produce_flags(flags)
+    if not flags.is_a?(Array)
+      flags = [flags]
+    end
+    flags.each {|flag|
+      id = flag[:id]
+      case flag[:op]
+      when :set
+        if !has_flag?(id)
+          @flags[id] = true
+        end
+      when :clear
+        if has_flag?(id)
+          @flags.delete(id)
+        end
+      when :inc
+        if has_flag?(id)
+          @flags[id] = @flags[id] + 1
+        else
+          @flags[id] = 1
+        end
+      when :dec
+        if has_flag?(id)
+          @flags[id] = @flags[id] - 1
+        else
+          @flags[id] = -1
+        end
+      end
+    }
   end
 
   def pay(company_money)
